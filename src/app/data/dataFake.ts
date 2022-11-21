068d4eaf677eda28867b24074486759e6ebe41ff
Romeu Reif~ <romeureif10@gmail.com>
2022-11-21 12:34:36 -0300

    added content and modified styles in content-style

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,92 @@
 export const dataFake = [
     {
         "id": "1",
-        "photo": "https://armazemdovovo.com.br/images/anuncios/avv-honda-(carros)-honda-civic-ex-1993-venda-gw9pag-seq-5-16-antigo-0640.jpg",
-        "title": "novo honda civic",
-        "description": "novo hondda lançado"
+        
+        "photo": "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202209/20220921/volkswagen-jetta-2.5-i-variant-20v-170cv-gasolina-4p-tiptronic-wmimagem17343722838.jpg?s=fill&w=1920&h=1440&q=75",
+       
+        "title": "Jetta Variant MK6 2011",
+        
+        "description": `Dá para dizer que a Jetta Variant é praticamente o Jetta de duas gerações atrás com um porta-malas maior e visual um pouco mais “careta”.
+
+        Na dianteira a perua segue o mesmo desenho do sedã. As mudanças começam da coluna “B” para trás, onde o modelo recebeu um teto com linha mais reta e uma maior área envidraçada devido ao porta-malas de 690 litros até o teto.
+        
+        A traseira é considerada bonita por alguns e feia por outros. Vamos combinar que as lanternas triangulares que invadem boa parte das laterais não combinam muito com o restante do conjunto.
+        
+        Tal unidade é capaz de desenvolver 170 cavalos de potência, a 5.000 rpm, e 24,5 kgfm de torque, a 4.250 rpm. A transmissão é uma automática Tiptronic de seis marchas.
+
+        A Volkswagen informa que a Jetta Variant pode acelerar de 0 a 100 km/h em 9 segundos. A velocidade máxima é de 205 km/h.`
     },
 
     {
         "id": "2",
-        "photo": "https://www.webmotors.com.br/imagens/prod/343383/SUBARU_IMPREZA_2.5_WRX_STI_SEDAN_4X4_16V_TURBO_INTERCOOLER_GASOLINA_4P_MANUAL_34338301473647850.webp?s=fill&w=236&h=135&q=70&t=true",
-        "title": "Honda civic type r",
-        "description": "O civic type-r vem ganhando vida"
-    }
+       
+        "photo": "https://http2.mlstatic.com/D_NQ_NP_650966-MLA52108518107_102022-W.jpg",
+       
+        "title": "Honda Civic 2002",
+        
+        "description": "Pois bem, esse G7 teve o primeiro motor nacionalizado da Honda com bloco de alumínio, um motor que teve vários cuidados com redução de atrito (para melhor consumo e menor poluição), um deles responsável por alguma redução também de durabilidade. Principalmente se o dono bobear quanto à qualidade e troca de lubrificante. Bem cuidado, este motor passa dos 200 mil km rodados, mas boa parte chega nesta quilometragem “batendo biela”. Entre os destaques, a suspensão com calibragem mais macia para atender as demandas da clientela, o porta-malas com maior capacidade e o assoalho plano para melhorar o conforto dos passageiros de trás. O motor 1.7 16V tinha potências de 115 cv e 130 cv e sugerimos as versões com transmissão automática."
+    },
+
+    {
+        "id": "3",
+        
+        "photo": "https://www.webmotors.com.br/imagens/prod/343383/SUBARU_IMPREZA_2.5_WRX_STI_SEDAN_4X4_16V_TURBO_INTERCOOLER_GASOLINA_4P_MANUAL_34338301473685514.webp?s=fill&w=440&h=330&q=80&t=true",
+        
+        "title": "Subaru Impreza 2021",
+        
+        "description":"O sedan esportivo chega à linha 2021 sem a WRX, ficando apenas a STI disponível. O motor disponível é o 2.5 turbo boxer de 310 cv e 41,5 kgfm, com transmissão manual e tração integral. Sua carroceria mede 4595 mm de comprimento, 1795 mm de largura, 1475 mm de altura e 2650 mm de entre-eixos, com porta-malas de 460 litros. O sedan atinge 100 km/h em 6 segundos e 240 km/h de velocidade máxima, e o STi faz em 5,2 segundos e chega a 250 km/h de máxima. Os principais itens de série do Impreza 2021 são 6 airbags, câmera de 360°, assistente de partida em rampa, faróis de Xenônio, vetorização de torque, diferencial central, piloto automático, sensor de chuva, sensor crepuscular, teto solar, chave presencial e sistema multimídia com espelhamento da tela do celular. A STi adiciona discos ventilados na traseira, ar condicionado de 2 zonas e aerofólio traseiro."
+    },
+
+    {
+        "id": "4",
+        
+        "photo": "https://cdn.motor1.com/images/mgl/zvOMK/s1/voz-do-dono-duplo-dois-proprietarios-falam-sobre-o-honda-new-civic-2007-e-2008.jpg",
+        
+        "title": "Honda Civic 2008 – New Civic",
+
+        "description":"Em abril de 2006 a marca japonesa sacudiu o mercado de sedãs médios com o chamado New Civic. Com desenho ousado e fora dos padrões mais tradicionais do segmento, o modelo se tornou quase um ícone da década no setor automotivo. Tanto que foi o que mais protagonizou disputas acirradas com o Corolla pela liderança da categoria – muitas vezes decididas por dezenas de unidades emplacadas. Entre as bossas do Honda Civic de oitava geração, o quadro de instrumentos em dois níveis, controle eletrônico de estabilidade para a versão topo de linha EXS e câmbio automático, agora com cinco velocidades, além do competente e eficiente motor 1.8 16V – que passou a ser flex já no fim do mesmo ano de lançamento. Por dentro, o acabamento ficou mais caprichado, os bancos mais confortáveis e o espaço interno melhor aproveitado. Mas o ambiente para bagagens era decepcionante, já que este Honda Civic foi projetado para ter estepe de uso temporário – o que ainda não era permitido pela legislação brasileira naqueles tempos. O jeito foi elevar o assoalho…"
+    },
+
+    {
+        "id": "5",
+        
+        "photo": "https://g1.globo.com/Noticias/Carros/foto/0,,29456040-FMM,00.jpg",
+        
+        "title": "Honda Civic Si 2009/2010",
+        
+        "description":"Coube ao New Civic estrear a versão Si, a mais esportiva do sedã médio feito no Brasil. O modelo foi o carro nacional mais potente da época – ok, na frieza dos números, os 192 cv só perdiam para o Volkswagen Golf “4,5” GTi quando este usava gasolina de alta octanagem e gerava 193 cv. Além do motor 2.0 i-VTEC, o Honda Civic Si trazia um conjunto com proposta realmente esportiva. A suspensão tinha suspensão mais firme, os freios eram dimensionados, o câmbio era manual de seis marchas e ainda havia bloqueio do diferencial. Os exemplares à venda são poucos e bem valorizados."
+    },
+
+    {
+        "id": "6",
+        
+        "photo": "https://cdn.autopapo.com.br/box/uploads/2021/09/08125425/honda-civic-lx-2020-1536x864.jpg",
+        
+        "title": "Honda Civic LX 2020",
+        
+        "description":"Para quem quer gastar um pouco menos e ter um carro mais novo e ainda dentro da garantia, uma pedida é a versão LX de entrada, que passou a integrar a linha 2020 desta décima geração do Honda Civic. O motor é o conhecido 2.0 aspirado flex de outros carnavais. Só que aliado a um câmbio CVT de sete marchas virtuais. Apesar de ser o modelo de entrada, segue a boa mecânica e ótima dirigibilidade da linha. É equipado com seis airbags, controles de estabilidade e tração, câmera de ré, ar-condicionado automático e rodas de liga leve aro 17”."
+    },
+
+    {
+        "id": "7",
+        
+        "photo": "https://img.olx.com.br/images/98/985206787989849.jpg",
+        
+        "title": "Honda Civic Touring 2018",
+        
+        "description":"A Honda voltou a ousar com o Civic na atual geração que conhecemos no Brasil. Com capô abaulado e traseira com caimento acentuado e lanternas futuristas, o sedã médio ficou bastante diferentão. Não se sabe se por isso, mas esta décima fase do modelo foi a que menos incomodou o Corolla em toda a história de embate entre os dois rivais. Uma pena, porque é um dos melhores Honda Civic da história. Tem acerto dinâmico apurado, câmbio CVT muito bem calibrado, que combina com o motor turbo de 173 cv eficiente e bastante disposto quando solicitado. Impressionante é a baixa desvalorização do carro. Mesmo com três anos de uso, custa mais que as versões de entrada da linha 0 km atual."
+    },
+
+    {
+        "id": "8",
+        
+        "photo": "https://revistacarro.com.br/wp-content/uploads/2018/05/bmw-3-series-gt.jpg",
+        
+        "title": "BMW 320 GT Sport 2016",
+        
+        "description":"O carro é sensacional, porque deriva da versão 320i Sport GP, tendo muito mais espaço e design diferenciado. Na relação custo/benefício é uma opção melhor que a 320i mais completa. Não é apenas o mesmo carro com uma modificação de carroceria na traseira. É, na verdade, outro carro construído sobre a mesma plataforma, mais alto, mais largo e comprido, sendo mais confortável no rodar. O painel frontal também é diferente, maior e mais alto. O espaço interno é maior que do série 5, chega a ser descomunal comparado com a 320 sedan. Na época que comprei a minha, queria a 320 sedan, mas na comparação vi que não era o mesmo carro. Sem dúvida. 320 sedan já é um excelente carro, mas a GT supera em muito o sedan. Claro que há uma questão de gosto pelo design da carroceria. Mas no uso prático do dia a dia, a GT é excelente, maior e mais imponente, afastando a sensação de confinamento do interior. É a opção mais acessível da carroceria Gran Turismo. Oferece mais versatilidade e porta-malas em relação ao sedã. O motor 2.0 biturbo em conjunto com o câmbio automático de 8 marchas formam uma excelente dupla. O desempenho do 320i GT é muito bom e surpreende. Sua dirigibilidade e comportamento dinâmico são exemplares, assim como no sedã."
+    },
+
+
 
 ]
