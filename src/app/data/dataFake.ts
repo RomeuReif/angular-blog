205cfcd452754d3ea980087bfe9c78f8c7dc82bd
Romeu Reif~ <romeureif10@gmail.com>
2022-11-21 07:39:38 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://armazemdovovo.com.br/images/anuncios/avv-honda-(carros)-honda-civic-ex-1993-venda-gw9pag-seq-5-16-antigo-0640.jpg",
+        "title": "novo honda civic",
+        "description": "novo hondda lançado"
+    },
+
+    {
+        "id": "2",
+        "photo": "https://www.webmotors.com.br/imagens/prod/343383/SUBARU_IMPREZA_2.5_WRX_STI_SEDAN_4X4_16V_TURBO_INTERCOOLER_GASOLINA_4P_MANUAL_34338301473647850.webp?s=fill&w=236&h=135&q=70&t=true",
+        "title": "Honda civic type r",
+        "description": "O civic type-r vem ganhando vida"
+    }
+
+]
